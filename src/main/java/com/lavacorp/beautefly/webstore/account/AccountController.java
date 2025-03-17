a18ba041844faf33343710c1cc1303aaf07b41a8
makan-kencing <dizzynight2096@gmail.com>
2025-03-17 22:43:54 +0800

    endpoint for account auth

--- src/main/java/com/lavacorp/beautefly/webstore/account/AccountController.java
+++ src/main/java/com/lavacorp/beautefly/webstore/account/AccountController.java
@@ -1,13 +1,26 @@
 package com.lavacorp.beautefly.webstore.account;
 
+import com.lavacorp.beautefly.webstore.account.dto.AccountLoginDTO;
 import com.lavacorp.beautefly.webstore.account.dto.AccountRegisterDTO;
 import com.lavacorp.beautefly.webstore.account.entity.Account;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.security.enterprise.identitystore.PasswordHash;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.hibernate.exception.ConstraintViolationException;
 
-import java.util.List;
+import java.util.Set;
 
 @Path("/account")
 @ApplicationScoped
@@ -16,19 +29,52 @@ public class AccountController {
     @Inject
     private AccountRepository accountRepository;
 
-    @GET
-    @Produces("application/json")
-    public List<Account> getAllAccounts() {
-        return accountRepository.findAll().toList();
+    @Inject
+    @Named("Argon2idPasswordHash")
+    private PasswordHash passwordHash;
+
+    @POST
+    @Path("/auth")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(AccountLoginDTO loginAccount, @Context HttpServletRequest req) {
+        try {
+            req.login(loginAccount.getEmail(), loginAccount.getPassword());
+        } catch (ServletException exc) {
+            return Response.status(Response.Status.FORBIDDEN).build();
+        }
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @RolesAllowed({"USER"})
+    @Path("/auth")
+    public Response logout(@Context HttpServletRequest req) {
+        try {
+            req.logout();
+        } catch (ServletException ignored) {
+
+        }
+        return Response.ok().build();
     }
 
     @POST
-    @Consumes("application/json")
-    public void register(AccountRegisterDTO account) {
-        var newUser = new Account();
-        newUser.setUsername(account.username);
-        newUser.setPassword(account.password);
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(AccountRegisterDTO registerAccount) {
+        try {
+            var account = new Account();
 
-        accountRepository.insert(newUser);
+            account.setUsername(registerAccount.getUsername());
+            account.setEmail(registerAccount.getEmail());
+            account.setPassword(passwordHash.generate(registerAccount.getPassword().toCharArray()));
+            account.setDob(registerAccount.getDob());
+
+            account.setRoles(Set.of(Account.Role.USER));
+
+            accountRepository.insert(account);
+
+            return Response.status(Response.Status.CREATED).build();
+        } catch (ConstraintViolationException exc) {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
     }
 }
