5b19728ec65d7a4aaed5567f1c8b23381f2d9697
makan-kencing <dizzynight2096@gmail.com>
2025-03-30 00:33:14 +0800

    dont extend CrudRepository. too many method for no reason at all

--- src/main/java/com/lavacorp/beautefly/webstore/account/AccountRepository.java
+++ src/main/java/com/lavacorp/beautefly/webstore/account/AccountRepository.java
@@ -2,9 +2,7 @@ package com.lavacorp.beautefly.webstore.account;
 
 import com.lavacorp.beautefly.webstore.account.entity.Account;
 import jakarta.annotation.Nullable;
-import jakarta.data.repository.CrudRepository;
-import jakarta.data.repository.Find;
-import jakarta.data.repository.Repository;
+import jakarta.data.repository.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -13,11 +11,20 @@ import java.util.List;
 
 @Transactional
 @Repository
-public interface AccountRepository extends CrudRepository<Account, Integer> {
+public interface AccountRepository {
+    @Insert
+    Account register(Account account);
+
     @Find
     List<Account> findByUsername(@NotBlank String username);
 
     @Find
     @Nullable
     Account findByEmail(@Email String email);
+
+    @Update
+    void update(Account account);
+
+    @Delete
+    void delete(Account account);;
 }
