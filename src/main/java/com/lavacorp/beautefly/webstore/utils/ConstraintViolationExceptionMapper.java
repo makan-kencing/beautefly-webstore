608ec559aa3ab53d98684ae02a1917c7ef1be51d
makan-kencing <dizzynight2096@gmail.com>
2025-03-31 21:48:30 +0800

    return json response for constraint exceptions

---
+++ src/main/java/com/lavacorp/beautefly/webstore/utils/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,33 @@
+package com.lavacorp.beautefly.webstore.utils;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.List;
+
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    @Override
+    public Response toResponse(ConstraintViolationException e) {
+        List<ValidationError> errors = e.getConstraintViolations()
+                .stream()
+                .map(this::toValidationError)
+                .toList();
+
+        return Response.status(ResponseStatus.UNPROCESSABLE_ENTITY)
+                .entity(errors)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    private ValidationError toValidationError(ConstraintViolation<?> constraintViolation) {
+        return new ValidationError(
+                constraintViolation.getPropertyPath().toString(),
+                constraintViolation.getMessage()
+        );
+    }
+}
