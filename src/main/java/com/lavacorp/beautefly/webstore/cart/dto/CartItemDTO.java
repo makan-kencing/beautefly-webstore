17afa91606822407802f7f83569e96e2d5723e30
makan-kencing <dizzynight2096@gmail.com>
2025-04-06 23:19:16 +0800

    add info to CartDtos

--- src/main/java/com/lavacorp/beautefly/webstore/cart/dto/CartItemDTO.java
+++ src/main/java/com/lavacorp/beautefly/webstore/cart/dto/CartItemDTO.java
@@ -2,5 +2,7 @@ package com.lavacorp.beautefly.webstore.cart.dto;
 
 import com.lavacorp.beautefly.webstore.product.entity.Product;
 
-public record CartItemDTO(Product product, int quantity) {
+import java.math.BigDecimal;
+
+public record CartItemDTO(Product product, int quantity, BigDecimal subtotal) {
 }
