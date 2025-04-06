b96fdf75f5d7bce2d5e4780bfdf00602d622e720
makan-kencing <dizzynight2096@gmail.com>
2025-04-06 21:24:42 +0800

    add `equals()` and `hashCode()` to `CartProduct` for use in Set

--- src/main/java/com/lavacorp/beautefly/webstore/cart/entity/CartProduct.java
+++ src/main/java/com/lavacorp/beautefly/webstore/cart/entity/CartProduct.java
@@ -11,9 +11,11 @@ import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -34,4 +36,20 @@ public class CartProduct implements Serializable {
     @CurrentTimestamp
     @PastOrPresent
     private Instant addedOn;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        CartProduct that = (CartProduct) o;
+        return getProduct() != null && Objects.equals(getProduct(), that.getProduct());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
