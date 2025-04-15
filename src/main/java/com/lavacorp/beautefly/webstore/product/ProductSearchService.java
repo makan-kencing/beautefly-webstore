f177f9df4b49c6af2e48a5f5e62a6931fc052f65
makan-kencing <dizzynight2096@gmail.com>
2025-04-16 00:57:58 +0800

    use criteria api to do dynamic search condition

--- src/main/java/com/lavacorp/beautefly/webstore/product/ProductSearchService.java
+++ src/main/java/com/lavacorp/beautefly/webstore/product/ProductSearchService.java
@@ -1,28 +1,65 @@
 package com.lavacorp.beautefly.webstore.product;
 
 import com.lavacorp.beautefly.webstore.product.dto.PaginatedResult;
-import com.lavacorp.beautefly.webstore.product.dto.ProductSearchContext;
+import com.lavacorp.beautefly.webstore.product.dto.ProductDTO;
 import com.lavacorp.beautefly.webstore.product.dto.ProductSearchDTO;
-import jakarta.data.Order;
-import jakarta.data.page.PageRequest;
+import com.lavacorp.beautefly.webstore.product.dto.ProductSearchResultDTO;
+import com.lavacorp.beautefly.webstore.product.entity.Product;
+import com.lavacorp.beautefly.webstore.product.mapper.ProductMapper;
+import jakarta.data.page.Page;
+import jakarta.data.page.impl.PageRecord;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Order;
 import jakarta.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.query.SelectionQuery;
+import org.hibernate.query.criteria.CriteriaDefinition;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+
+import java.util.stream.Stream;
 
 @ApplicationScoped
 @Transactional
 public class ProductSearchService {
-    @Inject
-    private ProductStatelessRepository statelessRepository;
+    @PersistenceUnit
+    private EntityManagerFactory emf;
 
-    public ProductSearchContext search(ProductSearchDTO search) {
-        var page = statelessRepository.findAllByNameLike(
-                search.query(),
-                PageRequest.ofPage(search.page(), search.pageSize(), true),
-                Order.by(search.sort().getCond())
+    @Inject
+    private ProductMapper productMapper;
+
+    public ProductSearchResultDTO search(ProductSearchDTO search) {
+        var builder = (HibernateCriteriaBuilder) emf.getCriteriaBuilder();
+
+        CriteriaQuery<Product> criteria = new CriteriaDefinition<>(emf, Product.class) {{
+            var root = from(Product.class);
+
+            select(root);
+            where(search.toPredicate(root, this, builder));
+
+            if (search.sort() != null)
+                orderBy((Order) search.sort().getOrder());
+        }};
+
+        SelectionQuery<Product> query = emf.createEntityManager()
+                .unwrap(Session.class)
+                .createSelectionQuery(criteria);
+
+        long total = query.getResultCount();
+        Stream<Product> products = query
+                .setFirstResult((search.page() - 1) * search.pageSize())
+                .setMaxResults(search.pageSize())
+                .getResultStream();
+        Page<ProductDTO> page = new PageRecord<>(
+                search.getPageRequest(),
+                products.map(productMapper::fromProduct).toList(),
+                total
         );
 
-        return new ProductSearchContext(
+        return new ProductSearchResultDTO(
                 PaginatedResult.fromPaginated(page),
                 search
         );
