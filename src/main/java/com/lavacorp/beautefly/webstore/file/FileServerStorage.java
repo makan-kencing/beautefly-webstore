f102e147e1cbfd896986fda82ae72a9c9db08430
makan-kencing <dizzynight2096@gmail.com>
2025-04-17 17:39:59 +0800

    set outputDir and baseHref from env

--- src/main/java/com/lavacorp/beautefly/webstore/file/FileServerStorage.java
+++ src/main/java/com/lavacorp/beautefly/webstore/file/FileServerStorage.java
@@ -1,6 +1,7 @@
 package com.lavacorp.beautefly.webstore.file;
 
-import com.lavacorp.beautefly.webstore.file.entity.File;
+import com.lavacorp.beautefly.env.ConfigurableEnvironment;
+import com.lavacorp.beautefly.env.env.EnvExpressionEvaluator;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.xml.bind.DatatypeConverter;
 import lombok.extern.log4j.Log4j2;
@@ -17,6 +18,8 @@ import java.security.NoSuchAlgorithmException;
 @Log4j2
 public class FileServerStorage implements FileStorage {
     private static final MessageDigest digester;
+    private static final Path outputDir;
+    private static final URI baseHref;
 
     static {
         try {
@@ -24,17 +27,19 @@ public class FileServerStorage implements FileStorage {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+
+        var evaluator = new EnvExpressionEvaluator();
+        var environment = new ConfigurableEnvironment(evaluator);
+
+        outputDir = Path.of(environment.getProperty("", "BEAUTEFLY_WEB_MEDIA_DIR"));
+        baseHref = URI.create(environment.getProperty("", "BEAUTEFLY_FILESERVER_BASE_URI"));
     }
-
-    static Path saveDir = Path.of("/var/www/data/media");
-    static URI baseHref = URI.create("/media");
-
     @Override
     public @Nullable String save(byte[] data, String extension) {
         var hash = DatatypeConverter.printHexBinary(digester.digest());
         var filename = hash + "." + extension;
 
-        Path filepath = saveDir.resolve(hash + extension);
+        Path filepath = outputDir.resolve(hash + extension);
 
         try {
             Files.write(filepath, data);
@@ -48,7 +53,7 @@ public class FileServerStorage implements FileStorage {
 
     @Override
     public boolean delete(String filename) {
-        Path localPath = saveDir.resolve(filename);
+        Path localPath = outputDir.resolve(filename);
 
         return localPath.toFile().delete();
     }
