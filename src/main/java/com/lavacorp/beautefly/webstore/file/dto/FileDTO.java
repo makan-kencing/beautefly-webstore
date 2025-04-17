714c6cf2df5faf420344cedc2b0f7832637e0437
makan-kencing <dizzynight2096@gmail.com>
2025-04-18 00:12:57 +0800

    get href for file dto

--- src/main/java/com/lavacorp/beautefly/webstore/file/dto/FileDTO.java
+++ src/main/java/com/lavacorp/beautefly/webstore/file/dto/FileDTO.java
@@ -1,10 +1,12 @@
 package com.lavacorp.beautefly.webstore.file.dto;
 
+import com.lavacorp.beautefly.webstore.file.FileServerStorage;
 import com.lavacorp.beautefly.webstore.file.entity.File;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
+import java.net.URI;
 
 /**
  * DTO for {@link File}
@@ -14,4 +16,7 @@ public record FileDTO(
         String filename,
         File.FileType type
 ) implements Serializable {
+    public URI href() {
+        return FileServerStorage.resolveRef(filename);
+    }
 }
