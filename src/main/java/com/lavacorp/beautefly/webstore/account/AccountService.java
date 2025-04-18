2e0de0547d2679ee70533c68b2cf2866c2d9fe22
makan-kencing <dizzynight2096@gmail.com>
2025-04-18 23:32:20 +0800

    use AccountService

---
+++ src/main/java/com/lavacorp/beautefly/webstore/account/AccountService.java
@@ -0,0 +1,27 @@
+package com.lavacorp.beautefly.webstore.account;
+
+import com.lavacorp.beautefly.webstore.account.dto.UserAccountDetailsDTO;
+import com.lavacorp.beautefly.webstore.account.mapper.AccountMapper;
+import com.lavacorp.beautefly.webstore.security.SecurityService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
+public class AccountService {
+    @Inject
+    private SecurityService securityService;
+
+    @Inject
+    private AccountMapper accountMapper;
+
+    public UserAccountDetailsDTO getUserAccountDetails(HttpServletRequest req) {
+        var account = securityService.getUserAccountContext(req);
+        if (account == null)
+            return null;
+
+        return accountMapper.toUserAccountDetailsDTO(account);
+    }
+}
--- src/main/java/com/lavacorp/beautefly/webstore/account/servlet/UserAccountServlet.java
+++ src/main/java/com/lavacorp/beautefly/webstore/account/servlet/UserAccountServlet.java
@@ -1,15 +1,34 @@
 package com.lavacorp.beautefly.webstore.account.servlet;
 
+import com.lavacorp.beautefly.webstore.account.AccountService;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
-@WebServlet("/useraccount")
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"USER", "STAFF", "ADMIN"}))
+@WebServlet("/account")
 public class UserAccountServlet extends HttpServlet {
+    @Inject
+    private AccountService accountService;
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/useraccount.jsp").forward(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var account = accountService.getUserAccountDetails(req);
+        if (account == null) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        req.setAttribute("account", account);
+
+        var view = req.getRequestDispatcher("/WEB-INF/views/account.jsp");
+        view.forward(req, resp);
     }
 }
