91f296e7fcd51e5c1d9b5ad526e19a72301f4483
makan-kencing <dizzynight2096@gmail.com>
2025-04-18 11:41:35 +0800

    add dtos 2

---
+++ src/main/java/com/lavacorp/beautefly/webstore/file/dto/UseFileDTO.java
@@ -0,0 +1,9 @@
+package com.lavacorp.beautefly.webstore.file.dto;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.lavacorp.beautefly.webstore.file.entity.File}
+ */
+public record UseFileDTO(int id) implements Serializable {
+}
