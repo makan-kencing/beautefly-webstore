0a0cb830da954b62311d20016c894e5f82131e12
makan-kencing <dizzynight2096@gmail.com>
2025-04-19 22:52:15 +0800

    login page redirect to last page after login

---
+++ src/main/java/com/lavacorp/beautefly/webstore/security/filter/PageVisitTracker.java
@@ -0,0 +1,56 @@
+package com.lavacorp.beautefly.webstore.security.filter;
+
+import jakarta.annotation.Nullable;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Queue;
+
+public class PageVisitTracker implements Filter {
+    public static String SESSION_ATTRIBUTE_NAME = "last-visits";
+    public static int LAST_N_VISIT = 2;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        var httpReq = (HttpServletRequest) req ;
+        var session = httpReq.getSession();
+
+        var queue = getQueue(session);
+        if (queue == null) {
+            queue = new CircularFifoQueue<>(LAST_N_VISIT);
+            session.setAttribute(SESSION_ATTRIBUTE_NAME, queue);
+        }
+
+        String uri = httpReq.getRequestURI();
+        String queryString = httpReq.getQueryString();
+        if (queryString != null)
+            uri += queryString;
+
+        queue.add(URI.create(uri));
+
+        chain.doFilter(req, resp);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static @Nullable Queue<URI> getQueue(HttpSession session) {
+        if (session.getAttribute(SESSION_ATTRIBUTE_NAME) instanceof Queue<?> queue)
+            if (queue.peek() instanceof URI)
+                return (Queue<URI>) queue;
+        return null;
+    }
+
+    public static @Nullable URI getLastUrl(HttpSession session) {
+        var urls = getQueue(session);
+        if (urls == null)
+            return null;
+
+        return urls.stream()
+                .skip(1)
+                .findFirst()
+                .orElse(null);
+    }
+}
--- src/main/java/com/lavacorp/beautefly/webstore/security/servlet/LoginServlet.java
+++ src/main/java/com/lavacorp/beautefly/webstore/security/servlet/LoginServlet.java
@@ -1,6 +1,9 @@
 package com.lavacorp.beautefly.webstore.security.servlet;
 
+import com.lavacorp.beautefly.webstore.security.filter.PageVisitTracker;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,10 +12,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/login")
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"USER", "STAFF" , "ADMIN"}))
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var view = req.getRequestDispatcher("WEB-INF/views/login.jsp");
-        view.forward(req, resp);
+        var session = req.getSession();
+        var url = PageVisitTracker.getLastUrl(session);
+
+        resp.sendRedirect(url != null ? url.toString() : "/");
     }
 }
