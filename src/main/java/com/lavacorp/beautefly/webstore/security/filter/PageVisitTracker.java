43b3a6e14d1799fd0a7106b37500518dd327e771
makan-kencing <dizzynight2096@gmail.com>
2025-04-20 21:24:55 +0800

    add ignorePatterns to PageVisitTracker with init params

--- src/main/java/com/lavacorp/beautefly/webstore/security/filter/PageVisitTracker.java
+++ src/main/java/com/lavacorp/beautefly/webstore/security/filter/PageVisitTracker.java
@@ -4,37 +4,74 @@ import jakarta.annotation.Nullable;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
+@Log4j2
 public class PageVisitTracker implements Filter {
-    public static String SESSION_ATTRIBUTE_NAME = "last-visits";
-    public static int LAST_N_VISIT = 3;
+    public static String SESSION_ATTRIBUTE_NAME = "visits";
+
+    public Pattern[] ignorePatterns = new Pattern[0];
+    public int queueSize = 3;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        var queueSize = filterConfig.getInitParameter("queueSize");
+        if (queueSize != null)
+            this.queueSize = Integer.parseUnsignedInt(queueSize);
+
+        var ignorePatterns = filterConfig.getInitParameter("ignorePatterns");
+        if (ignorePatterns != null)
+            this.ignorePatterns = ignorePatterns.lines()
+                    .map(String::strip)
+                    .map(Pattern::compile)
+                    .toArray(Pattern[]::new);
+    }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        var httpReq = (HttpServletRequest) req ;
+        var httpReq = (HttpServletRequest) req;
         var session = httpReq.getSession();
 
         var queue = getQueue(session);
         if (queue == null) {
-            queue = new CircularFifoQueue<>(LAST_N_VISIT);
+            queue = new CircularFifoQueue<>(queueSize);
             session.setAttribute(SESSION_ATTRIBUTE_NAME, queue);
         }
 
-        String uriString = httpReq.getRequestURI();
-        String queryString = httpReq.getQueryString();
-        if (queryString != null)
-            uriString += queryString;
-
-        URI uri = URI.create(uriString);
-        if (!uri.equals(queue.peek()))
-            queue.add(uri);
+        logUrl(queue, httpReq);
 
         chain.doFilter(req, resp);
+    }
+
+    private void logUrl(Queue<URI> queue, HttpServletRequest req) {
+        String uri = req.getRequestURI();
+        if (isIgnored(uri))
+            return;
+
+        String queryString = req.getQueryString();
+        if (queryString != null)
+            uri += queryString;
+
+        if (queue.peek() != null)
+            if (uri.equals(queue.peek().toString()))
+                return;
+
+        queue.add(URI.create(uri));
+        log.debug("Session: {}, visited {}.", req.getSession().getId(), uri);
+    }
+
+    private boolean isIgnored(String uri) {
+        return Arrays.stream(ignorePatterns)
+                .anyMatch(p -> p.matcher(uri).matches());
     }
 
     @SuppressWarnings("unchecked")
