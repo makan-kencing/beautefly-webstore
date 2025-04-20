16790ed4fbc313e408af69289fbdbe7924d209a9
CheeHua <cheehua03@gmail.com>
2025-04-20 20:11:40 +0800

    manage products page

---
+++ src/main/java/com/lavacorp/beautefly/webstore/product/servlet/ProductManager.java
@@ -0,0 +1,24 @@
+package com.lavacorp.beautefly.webstore.product.servlet;
+
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/admin/products")
+public class ProductManager extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+
+        var view = req.getRequestDispatcher("/WEB-INF/views/admin/manageProducts.jsp");
+        view.forward(req, resp);
+    }
+}
