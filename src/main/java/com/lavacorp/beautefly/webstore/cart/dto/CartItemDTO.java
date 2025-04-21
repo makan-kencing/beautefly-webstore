d8c8cc31ed22ff09ca0d9e84fe08f25e18ec57e1
makan-kencing <dizzynight2096@gmail.com>
2025-04-21 16:25:29 +0800

    fix wrong class reference

--- src/main/java/com/lavacorp/beautefly/webstore/cart/dto/CartItemDTO.java
+++ src/main/java/com/lavacorp/beautefly/webstore/cart/dto/CartItemDTO.java
@@ -1,11 +1,11 @@
 package com.lavacorp.beautefly.webstore.cart.dto;
 
-import com.lavacorp.beautefly.webstore.search.dto.ProductSearchDTO;
+import com.lavacorp.beautefly.webstore.search.dto.ProductSearchResultDTO;
 
 import java.math.BigDecimal;
 
 public record CartItemDTO(
-        ProductSearchDTO product,
+        ProductSearchResultDTO product,
         int quantity,
         BigDecimal subtotal
 ) {
