91c01e20d7d7087023f4f757ed11d6436cabc1ab
makan-kencing <dizzynight2096@gmail.com>
2025-04-21 09:25:43 +0800

    make creatdeat and updated at nullable

--- src/main/java/com/lavacorp/beautefly/webstore/common/entity/Auditable.java
+++ src/main/java/com/lavacorp/beautefly/webstore/common/entity/Auditable.java
@@ -1,7 +1,6 @@
 package com.lavacorp.beautefly.webstore.common.entity;
 
 import jakarta.persistence.MappedSuperclass;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,12 +14,10 @@ import java.time.Instant;
 @Setter
 @MappedSuperclass
 public class Auditable implements Serializable {
-    @NotNull
     @PastOrPresent
     @CurrentTimestamp
     protected Instant createdAt;
 
-    @NotNull
     @PastOrPresent
     @UpdateTimestamp
     protected Instant updatedAt;
