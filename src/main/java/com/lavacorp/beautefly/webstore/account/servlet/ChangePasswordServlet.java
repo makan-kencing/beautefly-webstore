a28492863844eb75aecdb97cbb1a207b33480df9
makan-kencing <dizzynight2096@gmail.com>
2025-04-21 22:08:55 +0800

    servlet to map the jsp

---
+++ src/main/java/com/lavacorp/beautefly/webstore/account/servlet/ChangePasswordServlet.java
@@ -0,0 +1,21 @@
+package com.lavacorp.beautefly.webstore.account.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/account/change-password")
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"*"}))
+public class ChangePasswordServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var view = req.getRequestDispatcher("/WEB-INF/views/change-password.jsp");
+        view.forward(req, resp);
+    }
+}
---
+++ src/main/java/com/lavacorp/beautefly/webstore/order/servlet/OrdersServlet.java
@@ -0,0 +1,21 @@
+package com.lavacorp.beautefly.webstore.order.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/orders")
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"*"}))
+public class OrdersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var view = req.getRequestDispatcher("/WEB-INF/views/orders.jsp");
+        view.forward(req, resp);
+    }
+}
