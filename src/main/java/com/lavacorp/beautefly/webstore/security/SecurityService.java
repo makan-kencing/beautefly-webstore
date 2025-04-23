c7e81811ca1412b14f9aacd8955aec7832a3344a
makan-kencing <dizzynight2096@gmail.com>
2025-04-23 20:47:03 +0800

    use em becoz stateless session dont cascade elementcollections

--- src/main/java/com/lavacorp/beautefly/webstore/security/SecurityService.java
+++ src/main/java/com/lavacorp/beautefly/webstore/security/SecurityService.java
@@ -5,29 +5,25 @@ import com.lavacorp.beautefly.webstore.security.dto.AccountRegisterDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.PersistenceUnit;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.security.enterprise.identitystore.PasswordHash;
 import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
-import org.hibernate.SessionFactory;
 import org.hibernate.exception.ConstraintViolationException;
 
 @Log4j2
 @Transactional
 @ApplicationScoped
 public class SecurityService {
-    @PersistenceUnit
-    private EntityManagerFactory emf;
+    @PersistenceContext
+    private EntityManager em;
 
     @Inject
     @Named("Argon2idPasswordHash")
     private PasswordHash passwordHash;
 
     public void register(AccountRegisterDTO registration) throws ConstraintViolationException {
-        var session = emf.unwrap(SessionFactory.class)
-                .openStatelessSession();
-
         var account = new Account();
 
         account.setUsername(registration.username());
@@ -36,6 +32,6 @@ public class SecurityService {
         var credential = account.getCredential();
         credential.setPassword(passwordHash.generate(registration.password().toCharArray()));
 
-        session.insert(account);
+        em.persist(account);
     }
 }
