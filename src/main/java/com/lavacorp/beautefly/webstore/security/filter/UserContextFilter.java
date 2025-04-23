809160cfed3f7b387d040cc1b0ebb2729de575d8
makan-kencing <dizzynight2096@gmail.com>
2025-04-23 13:28:24 +0800

    usercontextfilter directly fetching account details

--- src/main/java/com/lavacorp/beautefly/webstore/security/filter/UserContextFilter.java
+++ src/main/java/com/lavacorp/beautefly/webstore/security/filter/UserContextFilter.java
@@ -1,7 +1,9 @@
 package com.lavacorp.beautefly.webstore.security.filter;
 
+import com.lavacorp.beautefly.webstore.account.AccountRepository;
 import com.lavacorp.beautefly.webstore.account.mapper.AccountMapper;
-import com.lavacorp.beautefly.webstore.security.SecurityService;
+import com.lavacorp.beautefly.webstore.security.dto.AccountContextDTO;
+import jakarta.annotation.Nullable;
 import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,20 +11,38 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class UserContextFilter implements Filter {
+    public static final String ATTRIBUTE_NAME = "user";
+
     @Inject
-    private SecurityService securityService;
+    private AccountRepository accountRepository;
 
     @Inject
     private AccountMapper accountMapper;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        var httpReq = (HttpServletRequest) req;
+        var principal = ((HttpServletRequest) req).getUserPrincipal();
 
-        var user = securityService.getAccountContext(httpReq);
-        if (user != null)
-            httpReq.setAttribute("user", accountMapper.toAccountContextDTO(user));
+        if (principal != null) {
+            var account = accountRepository.findByEmail(principal.getName());
+
+            if (account.isPresent())
+                req.setAttribute(
+                        ATTRIBUTE_NAME,
+                        accountMapper.toAccountContextDTO(account.get())
+                );
+            else
+                ((HttpServletRequest) req).logout();
+        }
 
         chain.doFilter(req, resp);
+    }
+
+    public static @Nullable AccountContextDTO getUserContext(ServletRequest req) {
+        var value = req.getAttribute(ATTRIBUTE_NAME);
+        if (value == null)
+            return null;
+
+        return (AccountContextDTO) value;
     }
 }
