2e1d02f1190f54da220251e918bc3aed893925d9
makan-kencing <dizzynight2096@gmail.com>
2025-04-23 13:21:08 +0800

    swap to hibernate data repository

--- src/main/java/com/lavacorp/beautefly/webstore/account/AccountRepository.java
+++ src/main/java/com/lavacorp/beautefly/webstore/account/AccountRepository.java
@@ -4,29 +4,26 @@ import com.lavacorp.beautefly.webstore.account.entity.Account;
 import jakarta.annotation.Nullable;
 import jakarta.data.page.Page;
 import jakarta.data.page.PageRequest;
+import jakarta.data.repository.Find;
+import jakarta.data.repository.Repository;
+import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.processing.Pattern;
 import org.hibernate.query.Order;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
+@Transactional
 public interface AccountRepository {
-    @SuppressWarnings("UnusedReturnValue")
-    int register(Account account);
+    @Find
+    Optional<Account> find(int id);
 
-    @Nullable
-    Account findUserAccount(int id);
+    @Find
+    List<Account> findByUsernameLike(@NotBlank @Pattern String username);
 
-    List<Account> findByUsername(@NotBlank String username);
-
-    @Nullable
-    Account findByEmail(@Email String email);
-
-    Page<Account> findByUsernameLike(@NotBlank String username, PageRequest page, List<Order<? super Account>> orderBy);
-
-    void update(Account account);
-
-    void delete(Account account);
-
-    void deleteById(int id);
+    @Find
+    Optional<Account> findByEmail(@Email String email);
 }
