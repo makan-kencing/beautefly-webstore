c9b72b6859b1b24a59c6def4a995ff0737065976
makan-kencing <dizzynight2096@gmail.com>
2025-04-25 01:02:09 +0800

    create order dto and mappers

---
+++ src/main/java/com/lavacorp/beautefly/webstore/order/dto/OrderItemDTO.java
@@ -0,0 +1,23 @@
+package com.lavacorp.beautefly.webstore.order.dto;
+
+import com.lavacorp.beautefly.webstore.order.entity.SalesOrderProduct;
+import com.lavacorp.beautefly.webstore.product.dto.ProductPageDTO;
+import com.lavacorp.beautefly.webstore.search.dto.ProductSearchDTO;
+import com.lavacorp.beautefly.webstore.search.dto.ProductSearchResultDTO;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+/**
+ * DTO for {@link com.lavacorp.beautefly.webstore.order.entity.SalesOrderProduct}
+ */
+public record OrderItemDTO(
+        ProductSearchResultDTO product,
+        int quantity,
+        SalesOrderProduct.OrderProductStatus status,
+        BigDecimal unitPrice,
+        BigDecimal total,
+        Instant deliveredAt
+) implements Serializable {
+}
