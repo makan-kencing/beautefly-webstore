29919abeed2e98c12647c0327011c3f7f3de8573
makan-kencing <dizzynight2096@gmail.com>
2025-04-25 01:09:58 +0800

    delete ProductSearchContextDTO

--- src/main/java/com/lavacorp/beautefly/webstore/search/dto/ProductSearchContextDTO.java
+++
@@ -1,9 +0,0 @@
-package com.lavacorp.beautefly.webstore.search.dto;
-
-import com.lavacorp.beautefly.webstore.common.dto.PaginatedResult;
-
-public record ProductSearchContextDTO(
-        PaginatedResult<ProductSearchResultDTO> page,
-        ProductSearchDTO search
-) {
-}
