2f8f0f926ad0efec04f681d776053d29059c6fc2
makan-kencing <dizzynight2096@gmail.com>
2025-04-25 20:52:07 +0800

    use orderdto

---
+++ src/main/java/com/lavacorp/beautefly/webstore/admin/AdminOrderService.java
@@ -0,0 +1,39 @@
+package com.lavacorp.beautefly.webstore.admin;
+
+import com.lavacorp.beautefly.webstore.order.dto.OrderDetailsDTO;
+import com.lavacorp.beautefly.webstore.order.entity.SalesOrder;
+import com.lavacorp.beautefly.webstore.order.mapper.OrderMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+import jakarta.transaction.Transactional;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+@Transactional
+@ApplicationScoped
+public class AdminOrderService {
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @Inject
+    private OrderMapper orderMapper;
+
+    public List<OrderDetailsDTO> getOrders() {
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+
+        return session.createSelectionQuery("""
+                from SalesOrder so
+                    join fetch so.products
+                    join fetch so.shippingAddress
+                    join fetch so.account
+                    join fetch so.account.profileImage
+            """, SalesOrder.class)
+                .getResultStream()
+                .map(orderMapper::toOrderDetailsDTO)
+                .toList();
+    }
+}
--- src/main/java/com/lavacorp/beautefly/webstore/admin/servlet/AdminOrdersServlet.java
+++ src/main/java/com/lavacorp/beautefly/webstore/admin/servlet/AdminOrdersServlet.java
@@ -1,7 +1,6 @@
 package com.lavacorp.beautefly.webstore.admin.servlet;
 
-import com.lavacorp.beautefly.webstore.order.entity.OrderRepository;
-import com.lavacorp.beautefly.webstore.order.entity.SalesOrder;
+import com.lavacorp.beautefly.webstore.admin.AdminOrderService;
 import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,33 +9,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/admin/orders")
 public class AdminOrdersServlet extends HttpServlet {
-
     @Inject
-    private OrderRepository orderRepository;
+    private AdminOrderService adminOrderService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int pageNumber = 1;
-        int pageSize = 50;
-
-        String pageParam = req.getParameter("page");
-        if (pageParam != null && pageParam.matches("\\d+")) {
-            pageNumber = Integer.parseInt(pageParam);
-        }
-
-        int offset = (pageNumber - 1) * pageSize;
-
-        List<SalesOrder> orders = orderRepository.findPaged(offset, pageSize);
-        long totalOrders = orderRepository.count();
-        int totalPages = (int) Math.ceil((double) totalOrders / pageSize);
+        var orders = adminOrderService.getOrders();
 
         req.setAttribute("orders", orders);
-        req.setAttribute("currentPage", pageNumber);
-        req.setAttribute("totalPages", totalPages);
 
         var view = req.getRequestDispatcher("/WEB-INF/views/admin/orders.jsp");
         view.forward(req, resp);
