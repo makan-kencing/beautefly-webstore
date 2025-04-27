bdf35e63a07daa4b7b93227ba544cf2918d43d6f
makan-kencing <dizzynight2096@gmail.com>
2025-04-27 16:27:42 +0800

    move ProductCheckout.java to cart/ + use data from db

---
+++ src/main/java/com/lavacorp/beautefly/webstore/cart/servlet/CheckoutServlet.java
@@ -0,0 +1,39 @@
+package com.lavacorp.beautefly.webstore.cart.servlet;
+
+import com.lavacorp.beautefly.webstore.account.AccountService;
+import com.lavacorp.beautefly.webstore.cart.CartService;
+import com.lavacorp.beautefly.webstore.security.filter.UserContextFilter;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/checkout")
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"*"}))
+public class CheckoutServlet extends HttpServlet {
+    @Inject
+    private CartService cartService;
+
+    @Inject
+    private AccountService accountService;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var user = UserContextFilter.getUserContext(req);
+        assert user != null;
+
+        var cart = cartService.getCartDetails(req.getSession(), user);
+        var addresses = accountService.getAccountAddressesDetails(user);
+
+        req.setAttribute("cart", cart);
+        req.setAttribute("addresses", addresses);
+
+        var view = req.getRequestDispatcher("/WEB-INF/views/account/checkout.jsp");
+        view.forward(req, resp);
+    }
+}
