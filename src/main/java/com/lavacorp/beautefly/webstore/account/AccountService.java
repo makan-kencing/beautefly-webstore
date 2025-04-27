2ffe8e91e2863ecc59ec22f227e0174ec0d62e26
makan-kencing <dizzynight2096@gmail.com>
2025-04-27 18:28:49 +0800

    add address services

--- src/main/java/com/lavacorp/beautefly/webstore/account/AccountService.java
+++ src/main/java/com/lavacorp/beautefly/webstore/account/AccountService.java
@@ -5,6 +5,7 @@ import com.lavacorp.beautefly.webstore.account.dto.AddressesDTO;
 import com.lavacorp.beautefly.webstore.account.dto.UserAccountDetailsDTO;
 import com.lavacorp.beautefly.webstore.account.entity.Account;
 import com.lavacorp.beautefly.webstore.account.entity.Account_;
+import com.lavacorp.beautefly.webstore.account.entity.Address;
 import com.lavacorp.beautefly.webstore.account.entity.AddressBook_;
 import com.lavacorp.beautefly.webstore.account.mapper.AccountMapper;
 import com.lavacorp.beautefly.webstore.account.mapper.AddressMapper;
@@ -53,8 +54,77 @@ public class AccountService {
         var account = session.get(graph, GraphSemantic.FETCH, user.id());
 
         if (account == null)
-            throw new NoSuchElementException("Account id does not exists.");
+            throw new NoSuchElementException("Account does not exists.");
 
         return addressMapper.toAddressesDTO(account.getAddressBook());
+    }
+
+    public boolean checkAddressOwnership(int accountId, int addressId) {
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+
+        var count = session.createSelectionQuery("""
+                from Address
+                where account.id = :accountId
+                    and id = :addressId
+                """, Address.class)
+                .setParameter("accountId", accountId)
+                .setParameter("addressId", addressId)
+                .getResultCount();
+
+        return count > 0;
+    }
+
+    public AddressDTO getAddressDetails(int addressId) {
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+
+        var address = session.get(Address.class, addressId);
+
+        if (address == null)
+            throw new NoSuchElementException("Address does not exists.");
+
+        return addressMapper.toAddressDTO(address);
+    }
+
+    public void setDefaultAddress(AccountContextDTO user, int addressId) {
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+
+        var account = session.get(Account.class, user.id());
+
+        if (account == null)
+            throw new NoSuchElementException("Account does not exists.");
+
+        var address = new Address();
+        address.setId(addressId);
+
+        account.getAddressBook().setDefaultAddress(address);
+    }
+
+    public void updateAddressDetails(AddressDTO updatedAddress) {
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+
+        var address = session.get(Address.class, updatedAddress.id());
+
+        if (address == null)
+            throw new NoSuchElementException("Address does not exists.");
+
+        address = addressMapper.partialUpdate(updatedAddress, address);
+
+        session.update(address);
+    }
+
+    public void deleteAddress(int addressId) {
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+
+        var address = session.get(Address.class, addressId);
+
+        if (address == null)
+            throw new NoSuchElementException("Address does not exists.");
+
+        session.delete(address);
     }
 }
