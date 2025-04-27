59847cb15b4395572cc37e35fc3d08795e3ad0b6
makan-kencing <dizzynight2096@gmail.com>
2025-04-27 16:43:36 +0800

    create ProductItemId for embedded ids that represent a product item

--- src/main/java/com/lavacorp/beautefly/webstore/promotion/entity/PromotionProductId.java
+++ src/main/java/com/lavacorp/beautefly/webstore/promotion/entity/PromotionProductId.java
@@ -1,5 +1,6 @@
 package com.lavacorp.beautefly.webstore.promotion.entity;
 
+import com.lavacorp.beautefly.webstore.common.entity.ProductItemId;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Embeddable
-public class PromotionProductId {
+public class PromotionProductId extends ProductItemId {
     private int promotionId;
-    private int productId;
 }
