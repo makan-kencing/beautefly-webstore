fd9040e4cae2c8e439fe43819dc39e155204a37d
makan-kencing <dizzynight2096@gmail.com>
2025-04-28 16:33:46 +0800

    update rating servlets

--- src/main/java/com/lavacorp/beautefly/webstore/rating/servlet/RatingServlet.java
+++ src/main/java/com/lavacorp/beautefly/webstore/rating/servlet/RatingServlet.java
@@ -1,7 +1,13 @@
 package com.lavacorp.beautefly.webstore.rating.servlet;
 
+import com.lavacorp.beautefly.webstore.rating.RatingService;
+import com.lavacorp.beautefly.webstore.rating.mapper.RatingMapper;
+import com.lavacorp.beautefly.webstore.search.ProductSearchService;
+import com.lavacorp.beautefly.webstore.security.filter.UserContextFilter;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,17 +16,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@MultipartConfig
 @WebServlet("/rating")
 @ServletSecurity(@HttpConstraint(rolesAllowed = {"*"}))
 public class RatingServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var view = req.getRequestDispatcher("/WEB-INF/views/review.jsp");
-        view.forward(req, resp);
-    }
+    @Inject
+    private RatingService ratingService;
+
+    @Inject
+    private ProductSearchService productService;
+
+    @Inject
+    private RatingMapper ratingMapper;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int productId;
         try {
             productId = Integer.parseInt(req.getParameter("productId"));
@@ -28,6 +38,31 @@ public class RatingServlet extends HttpServlet {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
+
+        var product = productService.getProductDetailsById(productId);
+
+        req.setAttribute("product", product);
+
+        var view = req.getRequestDispatcher("/WEB-INF/views/review.jsp");
+        view.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var user = UserContextFilter.getUserContext(req);
+        assert user != null;
+
+        int productId;
+        try {
+            productId = Integer.parseInt(req.getParameter("productId"));
+        } catch (NullPointerException | NumberFormatException exc) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        var newRating = ratingMapper.toRatingNewDTO(req);
+
+        ratingService.rate(newRating, user);
 
         resp.sendRedirect("/product/" + productId);
     }
--- src/main/java/com/lavacorp/beautefly/webstore/rating/servlet/ReplyServlet.java
+++ src/main/java/com/lavacorp/beautefly/webstore/rating/servlet/ReplyServlet.java
@@ -1,8 +1,52 @@
 package com.lavacorp.beautefly.webstore.rating.servlet;
 
+import com.lavacorp.beautefly.webstore.rating.RatingService;
+import com.lavacorp.beautefly.webstore.rating.mapper.RatingMapper;
+import com.lavacorp.beautefly.webstore.security.filter.UserContextFilter;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.SessionFactory;
+
+import java.io.IOException;
 
 @WebServlet("/reply")
 public class ReplyServlet extends HttpServlet {
+    @Inject
+    private RatingMapper ratingMapper;
+
+    @Inject
+    private RatingService ratingService;
+
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var user = UserContextFilter.getUserContext(req);
+        assert user != null;
+
+        var newReply = ratingMapper.toReplyNewDTO(req);
+
+        // TODO: move into service or something
+        var session = emf.unwrap(SessionFactory.class)
+                .openStatelessSession();
+        var productId = session.createSelectionQuery("select product.id from Rating where id = :ratingId", Integer.class)
+                .setParameter("ratingId", newReply.originalId())
+                .getSingleResultOrNull();
+
+        if (productId == null) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        ratingService.reply(newReply, user);
+
+        resp.sendRedirect("/product/" + productId);
+    }
 }
