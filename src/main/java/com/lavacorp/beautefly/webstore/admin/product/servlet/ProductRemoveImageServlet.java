e22e84754315cfd32c894dd387b40d405613cff2
makan-kencing <dizzynight2096@gmail.com>
2025-05-01 15:33:00 +0800

    addd product remove and upload image servlets

---
+++ src/main/java/com/lavacorp/beautefly/webstore/admin/product/servlet/ProductRemoveImageServlet.java
@@ -0,0 +1,28 @@
+package com.lavacorp.beautefly.webstore.admin.product.servlet;
+
+import com.lavacorp.beautefly.webstore.admin.product.AdminProductService;
+import com.lavacorp.beautefly.webstore.admin.product.mapper.AdminProductMapper;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/product/image/upload")
+public class ProductRemoveImageServlet extends HttpServlet {
+    @Inject
+    private AdminProductService adminProductService;
+
+    @Inject
+    private AdminProductMapper adminProductMapper;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var dto = adminProductMapper.toRemoveProductImageDTO(req);
+
+        adminProductService.removeProductImage(dto);
+    }
+}
---
+++ src/main/java/com/lavacorp/beautefly/webstore/admin/product/servlet/ProductUploadImageServlet.java
@@ -0,0 +1,32 @@
+package com.lavacorp.beautefly.webstore.admin.product.servlet;
+
+import com.lavacorp.beautefly.webstore.admin.product.AdminProductService;
+import com.lavacorp.beautefly.webstore.admin.product.mapper.AdminProductMapper;
+import com.lavacorp.beautefly.webstore.security.filter.UserContextFilter;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/product/image/remove")
+public class ProductUploadImageServlet extends HttpServlet {
+    @Inject
+    private AdminProductService adminProductService;
+
+    @Inject
+    private AdminProductMapper adminProductMapper;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var user = UserContextFilter.getUserContext(req);
+        assert user != null;
+
+        var dto = adminProductMapper.toUploadProductImageDTO(req);
+
+        adminProductService.uploadProductImage(user, dto);
+    }
+}
