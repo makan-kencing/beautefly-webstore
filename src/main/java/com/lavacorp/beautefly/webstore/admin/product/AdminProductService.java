bee23974a7270f70decb81ca0e38bc56f4354dbc
makan-kencing <dizzynight2096@gmail.com>
2025-05-01 21:22:45 +0800

    update image remove to actually works

--- src/main/java/com/lavacorp/beautefly/webstore/admin/product/AdminProductService.java
+++ src/main/java/com/lavacorp/beautefly/webstore/admin/product/AdminProductService.java
@@ -20,6 +20,7 @@ import org.hibernate.graph.GraphSemantic;
 
 import java.io.IOException;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Transactional
 @ApplicationScoped
@@ -153,14 +154,20 @@ public class AdminProductService {
         var session = emf.unwrap(SessionFactory.class)
                 .openStatelessSession();
 
-        session.createMutationQuery("""
-                            delete Product.images
-                            where Product.id = :productId
-                            and FileUpload.id = :imageId
-                        """)
-                .setParameter("productId", dto.id())
-                .setParameter("imageId", dto.imageId())
-                .executeUpdate();
+        var graph = session.createEntityGraph(Product.class);
+        graph.addPluralSubgraph(Product_.images);
+
+        var product = session.get(graph, GraphSemantic.FETCH, dto.id());
+        if (product == null)
+            return;
+
+        product.setImages(
+                product.getImages().stream()
+                        .filter(i -> i.getId() != dto.imageId())
+                        .collect(Collectors.toSet())
+        );
+
+        session.update(product);
     }
 
     public CreateProductContext getCreateProductContext() {
